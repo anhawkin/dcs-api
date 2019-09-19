dac6302ba220484961ed3eab0b0b059a047cbd59
anhawkin <anhawkin@adobe.com>
2019-09-19 15:44:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var request = require('request');
 
 app.get('/', (req, res) => {
 
-	request('https://nemeascshared.demdex.net/event?d_cid_ic=crm_data%01YW5oYXdraW5AYWRvYmUuY29tMVRlbGVncmFwaDE3LzkvMjAxOQ&c_customerstatus=vip', function (error, response, body) {
+	request('https://yourcompany.demdex.net/event?d_cid_ic=crm_data%01YW5oYXdrbW5AYWRvYmUuY29tMVRlbGVncmFwaDE3LzkvMjAxOQ&c_customerstatus=vip', function (error, response, body) {
 	  console.log('error:', error); // Print the error if one occurred
 	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 	});
-  	res.send('Hello World!')
+  	res.send('You just made a request to DCS API!')
 });
 
 app.listen(8000, () => {
